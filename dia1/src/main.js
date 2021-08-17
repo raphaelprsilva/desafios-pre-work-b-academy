16e0327d6f5ed514f675a46faa60a17d1b2f478c
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-08-17 20:09:59 -0300

    Add exercises 02, 03, 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,9 @@
-import './style.css'
+const link = document.querySelector('[data-js="change-visibility"]');
+const div = document.querySelector('[data-js="app"]');
 
-document.querySelector('#app').innerHTML = `
-  <h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+link.addEventListener("click", (event) => {
+  event.preventDefault();
+  div.style.display === "none"
+    ? (div.style.display = "block")
+    : (div.style.display = "none");
+  }, false);
